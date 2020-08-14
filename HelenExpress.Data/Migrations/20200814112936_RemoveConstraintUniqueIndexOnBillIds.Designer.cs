7789aeb1e0303169b6f279f8d027cd87ebdbae40
Hieu Tran <trantrunghieu3010@gmail.com>
2020-08-14 18:32:30 +0700

    Remove constraint unique on AirlineBillId and ChildBillId

--- HelenExpress.Data/Migrations/20200814112936_RemoveConstraintUniqueIndexOnBillIds.Designer.cs
+++ HelenExpress.Data/Migrations/20200814112936_RemoveConstraintUniqueIndexOnBillIds.Designer.cs
@@ -4,15 +4,17 @@ using HelenExpress.Data;
 using HelenExpress.Data.JSONModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HelenExpress.Data.Migrations
 {
     [DbContext(typeof(HeLenExpressDbContext))]
-    partial class HeLenExpressDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200814112936_RemoveConstraintUniqueIndexOnBillIds")]
+    partial class RemoveConstraintUniqueIndexOnBillIds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HelenExpress.Data/Migrations/20200814112936_RemoveConstraintUniqueIndexOnBillIds.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HelenExpress.Data.Migrations
+{
+    public partial class RemoveConstraintUniqueIndexOnBillIds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_bill_airlinebillid",
+                table: "bill");
+
+            migrationBuilder.DropIndex(
+                name: "ix_bill_childbillid",
+                table: "bill");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bill_airlinebillid",
+                table: "bill",
+                column: "airlinebillid");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bill_childbillid",
+                table: "bill",
+                column: "childbillid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_bill_airlinebillid",
+                table: "bill");
+
+            migrationBuilder.DropIndex(
+                name: "ix_bill_childbillid",
+                table: "bill");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bill_airlinebillid",
+                table: "bill",
+                column: "airlinebillid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bill_childbillid",
+                table: "bill",
+                column: "childbillid",
+                unique: true);
+        }
+    }
+}
