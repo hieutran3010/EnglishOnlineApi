85fa0b49816619b7110802a3fdb3f72d51b8ad99
Hieu Tran <trantrunghieu3010@gmail.com>
2020-08-28 09:43:58 +0700

    Update bill delivery history

--- HelenExpress.Data/JSONModels/BillDeliveryHistory.cs
+++ HelenExpress.Data/JSONModels/BillDeliveryHistory.cs
@@ -5,7 +5,7 @@ namespace HelenExpress.Data.JSONModels
     public class BillDeliveryHistory
     {
         public DateTime? Date { get; set; }
-        public TimeSpan? Time { get; set; }
+        public DateTime? Time { get; set; }
         public string Status { get; set; }
     }
 }
